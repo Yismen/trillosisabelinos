4e74b1a5f2514910857a2f2d9e01c7a197f251e7
Yismen Jorge <yismen.jorge@gmail.com>
2023-06-27 13:56:19 -0400

    refactor: Refactor welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -140,27 +140,43 @@
 
         <section class="container showcases with-border" id="showcase">
             <h3 class="title">Pura Diversión, <span class="text-highlighted">Momentos</span> Inolvidables </h3>
-            <div class="content row  py-3 justify-content-center align-items-center">
-                <x-showcase class="col-md-6 col-lg-3 mb-3" title="Edición 2019"
-                    :image="asset('images/showcase/showcase-1.jpeg')">
-                    Miguel Garcia, disfrutando un Mabí, bebida insígnia de Trillos Isabelinos.
+            <div class="content row  py-3 justify-content-center align-items-center gap-3">
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center"
+                    title="Equipo de Trabajo" :image="asset('images/showcase/equipo-2.jpg')">
+                    Edicion 2019, los matatanes.
                 </x-showcase>
-                <x-showcase class="col-md-6 col-lg-3 mb-3" title="Edición 2019"
-                    :image="asset('images/showcase/showcase-1.jpeg')">
-                    Miguel Garcia, disfrutando un Mabí, bebida insígnia de Trillos Isabelinos.
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center"
+                    title="Equipo de Trabajo" :image="asset('images/showcase/equipo-1.jpg')">
+                    Siempre listos, dedicados a servir, trato humano, calidad de primera.
                 </x-showcase>
-                <x-showcase class="col-md-6 col-lg-3 mb-3" title="Edición 2019"
-                    :image="asset('images/showcase/showcase-1.jpeg')"></x-showcase>
-                <x-showcase class="col-md-6 col-lg-3 mb-3" title="Edición 2019"
-                    :image="asset('images/showcase/showcase-1.jpeg')"></x-showcase>
-                <x-showcase class="col-md-6 col-lg-3 mb-3" title="Edición 2019"
-                    :image="asset('images/showcase/showcase-1.jpeg')"></x-showcase>
-                <x-showcase class="col-md-6 col-lg-3 mb-3" title="Edición 2019"
-                    :image="asset('images/showcase/showcase-1.jpeg')"></x-showcase>
-                <x-showcase class="col-md-6 col-lg-3 mb-3" title="Edición 2019"
-                    :image="asset('images/showcase/showcase-1.jpeg')"></x-showcase>
-                <x-showcase class="col-md-6 col-lg-3 mb-3" title="Edición 2019"
-                    :image="asset('images/showcase/showcase-1.jpeg')"></x-showcase>
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center" title="Nicky Suero"
+                    :image="asset('images/showcase/banderazo-1.jpg')">
+                    Palabras inaugurales y banderazo de salida.
+                </x-showcase>
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center" title="Hay Mabí"
+                    :image="asset('images/showcase/aneudy-disfrutando-un-mabi.jpg')">
+                    Aneudy, disfrutando de un refrescante Mabi, bebida insignia de Trillos Isabelinos!
+                </x-showcase>
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center" title="Cero Calor"
+                    :image="asset('images/showcase/refrescantes-1.jpg')">
+                    Refrescantes bebidas y abastecimientos de primera calidad!
+                </x-showcase>
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center" title="Patron Bike!"
+                    :image="asset('images/showcase/enmanuel-y-su-competidor-patron-bike.jpg')">
+                    Enmanuel y su competidor de Patron Bike
+                </x-showcase>
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center" title="Bicicentro!"
+                    :image="asset('images/showcase/andres-valerio-representante-bicicentro.jpg')">
+                    Andres Valerio, representando Bicicentro
+                </x-showcase>
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center" title="Ciclon Bike!"
+                    :image="asset('images/showcase/wendy-cruz-ciclon-bike-aneudy.jpg')">
+                    Wendy Cruz, representando Ciclon Bike
+                </x-showcase>
+                <x-showcase class="col-sm-6 col-md-4 col-lg-3 mb-3 d-flex justify-content-center" title="Entre Amigos!"
+                    :image="asset('images/showcase/entre-amigos-constanza.jpg')">
+                    Grupo Entre Amigos de Constanza, siempre presentes
+                </x-showcase>
             </div>
         </section>
 
