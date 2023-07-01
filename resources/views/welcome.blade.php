7c3199a117b5b6ba771acba7252fecb807a0ea21
Yismen Jorge <yismen.jorge@gmail.com>
2023-07-01 16:34:45 -0400

    Add google analystic

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -433,6 +433,15 @@
         }); 
         })()
     </script>
+
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-74T5P7Y73B"></script>
+    <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-74T5P7Y73B');
+    </script>
 </body>
 
 </html>
