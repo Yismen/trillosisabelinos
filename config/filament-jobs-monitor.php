5ed838ff2d80adeb15529539c588195d3d211dae
Yismen Jorge <yismen.jorge@gmail.com>
2023-07-06 15:25:29 -0400

    Add jobs monitor endpoint

---
+++ config/filament-jobs-monitor.php
@@ -0,0 +1,14 @@
+<?php
+
+// config for Croustibat/FilamentJobsMonitor
+return [
+    'navigation' => [
+        'enabled' => true,
+        'group_label' => 'Backend',
+        'icon' => 'heroicon-o-chip',
+    ],
+    'pruning' =>  [
+        'activate' => true,
+        'retention_days' => 7,
+    ]
+];
