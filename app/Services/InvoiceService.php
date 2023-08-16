e048cfed3ac16140aa381434ba654e13f8adea03
Yismen Jorge <yismen.jorge@gmail.com>
2023-08-16 15:40:32 -0400

    Add ability to view payment invoice images

--- app/Services/InvoiceService.php
+++ app/Services/InvoiceService.php
@@ -3,7 +3,23 @@
 namespace App\Services;
 
 use LaravelDaily\Invoices\Invoice;
+use Symfony\Component\HttpFoundation\File\File;
 
 class InvoiceService extends Invoice
 {
+    public $images;
+
+    public function images(array | null $images)
+    {
+        $this->images = $images;
+
+        return $this;
+    }
+
+    public function getImage(string $url)
+    {
+        $file = new File("storage/" . $url);
+
+        return 'data:' . $file->getMimeType() . ';base64,' . base64_encode($file->getContent());
+    }
 }
